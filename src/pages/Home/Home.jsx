1bdcf4608895b0e1c7a04d98aa504c2155e5c375
Nayem <nayemkhan4496@gmail.com>
2023-07-24 14:52:03 -0400

    project intialize

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,9 +2,32 @@ import { useState } from 'react';
 import Footer from '../../components/Navbar/Footer/Footer';
 
 const Home = () => {
-    const [audioUrl, setAudioUrl] = useState(null)
-    const [masterAudioUrl, setMasterAudioUrl] = useState(null);
-    const [childFile, setChildFile] = useState(null);
+    const [childAudioDataList, setChildAudioDataList] = useState([
+        {
+            "url": "sdfsdf",
+            "min": 23,
+            "sec": 35
+        },
+        {
+            "url": "sdfsdf",
+            "min": 23,
+            "sec": 35
+        },
+        {
+            "url": "sdfsdf",
+            "min": 23,
+            "sec": 35
+        },
+        {
+            "url": "sdfsdf",
+            "min": 23,
+            "sec": 35
+        }
+    ]);
+    let childAudioUrl = null;
+    let [masterAudioUrl, setMasterAudioUrl] = useState();
+    let [youtubeApi, setYoutubeApi] = useState();
+    let masterClicked = false;
 
     const myWidget = window.cloudinary.createUploadWidget(
         {
@@ -14,41 +37,33 @@ const Home = () => {
         (error, result) => {
             console.log(result)
             if (!error && result && result.event === "success") {
-                console.log(result.info.secure_url);
-                setAudioUrl(result?.info?.secure_url);
+                const url = result.info.secure_url;
+                console.log(url);
+                if (masterClicked) {
+                    setMasterAudioUrl(url);
+                } else {
+                    childAudioUrl = url;
+                }
             }
         }
     );
 
-    const masterFileHandler = () => {
-        setMasterAudioUrl(audioUrl);
-        setAudioUrl(null);
-    }
-
-    const childFileHandler = () => {
-        setChildFile(audioUrl);
-        audioUrl(null);
-    }
     const submitHandler = (event) => {
         event.preventDefault()
-       
+
         const timeMinute = event.target.minute.value;
         const timeSecond = event.target.second.value;
         const youtubeAPI = event.target.youtubeAPI.value;
 
         const allInfo = {
-            masterFileData: {
-                secure_url: masterAudioUrl,
-            },
-            childFileData: {
-                secure_url: childFile,
-            },
+            masterAudioUrl,
+            childFileData: childAudioDataList,
             timeMinute,
             timeSecond,
             youtubeAPI,
         }
 
-        
+
 
         fetch("http://localhost:5000/info", {
             method: "POST",
@@ -57,62 +72,61 @@ const Home = () => {
             },
             body: JSON.stringify(allInfo),
         })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((er) => console.log(er));
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((er) => console.log(er));
 
+    }
+    const addHandler = (event) => {
+        event.preventDefault()
+        setYoutubeApi(event.target.youtubeAPI.value)
+    }
+    const addChildHandler = (event) => {
+        event.preventDefault()
+        setYoutubeApi(event.target.youtubeAPI.value)
     }
     return (
         <div className='mt-28'>
             <div className='bg-[#2e313d] w-1/2 mx-auto py-10 rounded-xl'>
                 <div className='flex flex-col justify-center items-center'>
-                    <form onSubmit={submitHandler} encType='multipart/form-data'>
-                        <div className="form-control w-full max-w-xs">
-                            <label className="label">
-                                <span className="label-text text-center">Pick a master audio file</span>
-                            </label>
-                            <input
-                                
-                                type="file"
-                                name="masterFile" 
-                                className="file-input file-input-bordered w-full max-w-xs" />
-                        </div>
-                        <div onClick={() => myWidget.open()}>
-                            <button onClick={masterFileHandler}>Upload</button>
-                        </div>
-                        <div className="form-control w-full max-w-xs mt-5">
-                            <label className="label">
-                                <span className="label-text ">Pick a child audio file</span>
-                            </label>
-                            <input 
-                                type="file"
-                                name="childrenFile" 
-                                className="file-input file-input-bordered w-full max-w-xs" />
-                            <label className="label">
-                                <span className="label-text ">Inserted time</span>
-                            </label>
-                            <div className='flex'> 
-                                <input type="text" name='minute' placeholder="0 minute" className="input input-bordered w-full max-w-xs mr-4" required/>
-                                <input type="text" name='second' placeholder="0 second" className="input input-bordered w-full max-w-xs" required/>
+                    {masterAudioUrl && youtubeApi && ( <>
+                            <div>{masterAudioUrl}</div>
+                            <div>{youtubeApi}</div>
+                            <form onSubmit={submitHandler} encType='multipart/form-data'>
+                                <div className="form-control w-full max-w-xs mt-5">
+                                    <label className="label">
+                                        <span className="label-text ">Pick a child audio file</span>
+                                    </label>
+                                    <button onClick={() => {
+                                        myWidget.open();
+                                        masterClicked = false;
+                                    }}>Select Audio</button>
+                                    <input type="text" name='minute' placeholder="0 minute" className="input input-bordered w-full max-w-xs mr-4" required/>
+                                    <input type="text" name='second' placeholder="0 second" className="input input-bordered w-full max-w-xs" required/>
+                                </div>
+
+                                <div className='w-full text-center mt-5'>
+                                    <input className="btn btn-outline" type="submit" value="Add Child" />
+                                </div>
+                            </form>
+                        </>
+                    )}
+                    {(!masterAudioUrl || !youtubeApi) && (<>
+                        <form onSubmit={addHandler} encType='multipart/form-data'>
+                            <button onClick={() => {
+                                myWidget.open();
+                                masterClicked = true;
+                            }}>Pick a master audio file</button>
+                            <div className="form-control w-full max-w-xs mt-5">
+                                <input type="text" name='youtubeAPI' placeholder="Youtube API" className="input input-bordered w-full max-w-xs" required/>
                             </div>
-                        </div>
-
-                        <div onClick={() => myWidget.open()}>
-                            <button onClick={childFileHandler}>Upload</button>
-                        </div>
-
-                        <div className="form-control w-full max-w-xs mt-5">
-                            <label className="label">
-                                <span className="label-text">Youtube API</span>
-                            </label>
-                            <input type="text" name='youtubeAPI' placeholder="Youtube API" className="input input-bordered w-full max-w-xs" required/>
-                        </div>
-                        <div className='w-full text-center mt-5'>
-                            <input className="btn btn-outline" type="submit" value="submit" />
-                        </div>
-                    </form>
+                            <div className='w-full text-center mt-5'>
+                                <input className="btn btn-outline" type="submit" value="Add" />
+                            </div>
+                        </form>
+                    </>)}
                 </div>
             </div>
             <Footer></Footer>
